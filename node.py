d68a2060f53e5f430829b437234544cd36ce35d3
Eric Finn <ericfinn.166@gmail.com>
2012-02-16 16:53:44 -0500

    Added toStr function, modified print to use it.

--- node.py
+++ node.py
@@ -1,16 +1,28 @@
 #! /usr/bin/env python
 
 class Node():
+	#parent: Node
+	#children: dict((attribute value) -> Node)
+	#classification: attribute value (of the classification attribute)
+	#attribute: tuple(string, set(attribute value))
     def __init__(self, parent, children, classification, attribute):
         self.parent = parent
         self.children = children
         self.classification = classification
         self.attribute = attribute
 
+	#indent: number - number of spaces to indent this node by
     def print(self):
+        print self.toStr(0)
+
+	def toStr(self, indent):
+		str = ""
+		nextIndent = indent + 2
         if(self.classification != None):
-            print self.classification
+            return indent*" "+"{}".format(self.classification)
         elif(self.attribute != None):
-            print "{}?".format(self.attribute)
-        for child in self.children:
-            child.print()
+            str = indent*" "+"{}? : {\n".format(self.attribute)
+        for val, child in self.children:
+			#print value : node
+			str = str + nextIndent*" " + val + " :\n" + child.toStr(nextIndent) + ",\n"
+		str = str + indent*" "+"}"
